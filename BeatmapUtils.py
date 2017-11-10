4b765a5a37156ee8655dbb64ee9c0b16d586dc32
Awlex <wieseralex580@gmail.com>
2017-11-10 20:10:25 +0100

    Fixed parsing and added a test

--- BeatmapUtils.py
+++ BeatmapUtils.py
@@ -6,18 +6,18 @@ Data structure
 """
 
 
-def circle(raw_hitobject, hitobjects):
+def _circle(raw_hitobject, hitobjects):
     hitobjects.append(','.join(raw_hitobject.split(',')[:4]))
 
 
-def slider(raw_hitobject, hitobjects):
+def _slider(raw_hitobject, hitobjects):
     split = raw_hitobject.split(',')
     first = ','.join(split[:4])
     second = ','.join(split[5:8])
     hitobjects.append("%s,%s" % (first, second))
 
 
-def spinner(raw_hitobject, hitobjects):
+def _spinner(raw_hitobject, hitobjects):
     split = raw_hitobject.split(',')
     hitobjects.append(','.join(split[:4] + ',%s' % split[-1].split(':')[0]))
 
@@ -27,26 +27,26 @@ def progress_raw_items(raw_hitobjects):
 
     # Each appends its contents individually
     for raw_hitobject in raw_hitobjects:
-        type = raw_hitobject.split(',')[3]
+        type = int(raw_hitobject.split(',')[3])
         if type & 1:
-            return circle(raw_hitobject, hitobjects)
+            _circle(raw_hitobject, hitobjects)
         elif type & 2:
-            return slider(raw_hitobject, hitobjects)
+            _slider(raw_hitobject, hitobjects)
         elif type & 8:
-            return spinner(raw_hitobject, hitobjects)
+            _spinner(raw_hitobject, hitobjects)
 
     return hitobjects
 
 
 def get_hitobject_data(osu_file):
     # Read All lines
-    with open(osu_file) as f:
+    with open(osu_file, encoding='utf8') as f:
         hitobjects = f.readlines()
 
     # Read where the hitobjects start
     index = -1
-    for line, index in enumerate(hitobjects):
-        if line == '[HitObjects]':
+    for index, line in enumerate(hitobjects):
+        if line.__contains__('[HitObjects]'):
             break
 
     # Abort if there was no start found
---
+++ test/BeatmapUtilsTest.py
@@ -0,0 +1,8 @@
+from BeatmapUtils import get_hitobject_data
+
+hitobjects = get_hitobject_data('testfile.osu')  # Drop - Granat, because it's a short one
+
+print('Length:', len(hitobjects))
+print('Head:', hitobjects[:5])
+print('Tail:', hitobjects[-5:])
+
