88f8337701e5c514c4f23ae6c9d45eab9c37089c
Awlex <wieseralex580@gmail.com>
2017-11-10 22:34:14 +0100

    Defined read functions for database

--- OsuDBReader.py
+++ OsuDBReader.py
@@ -1,4 +1,74 @@
-def fetch_n_ranked_maps(n=1000, min_sr=None, max_sr=None, random=True, game_mode='std', use_exported=True, export_list=True,
+class OsuDB:
+    def __init__(self, file=None):
+        self.file = open(file)
+
+    def read_byte(self):
+        """
+        Read one Byte from the database-file
+        :param file:
+        :return:
+        """
+        pass
+
+    def read_short(self):
+        """
+        Read a Short (2 Byte) from the database-file
+        :return:
+        """
+        pass
+
+    def read_int(self):
+        """
+        Read a Integer (4 Bytes) from the database-file
+        :return:
+        """
+        pass
+
+    def read_long(self):
+        """
+        Read a Long (8 bytes) from the database-file
+        :return:
+        """
+        pass
+
+    def read_uleb128(self):
+        """
+        Read a uleb128 (variable) from the database-file
+        :return:
+        """
+        pass
+
+    def read_single(self):
+        """
+        Read a Single (4 bytes) from the database-file
+        :return:
+        """
+        pass
+
+    def read_double(self):
+        """
+        Read a Double (8 bytes) from the database-file
+        :return:
+        """
+        pass
+
+    def read_boolean(self):
+        """
+        Read a Boolean (1 byte) from the database-file
+        :return:
+        """
+        pass
+
+    def read_string(self):
+        """
+        Read a string (variable) from the database-file
+        :return:
+        """
+        pass
+
+
+def fetch_n_ranked_maps(n=1000, min_sr=None, max_sr=None, random=True, game_mode='std', use_exported=True,
+                        export_list=True,
                         filename='exported_maps.pkl'):
     """
 
