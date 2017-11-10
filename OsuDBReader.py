e9e46c9f90b25d245029bb1249c28037c111d8bd
Awlex <wieseralex580@gmail.com>
2017-11-10 21:24:21 +0100

    Added Basic parsing

---
+++ OsuDBReader.py
@@ -0,0 +1,14 @@
+def fetch_n_ranked_maps(n=1000, min_sr=None, max_sr=None, random=True, use_exported=True, export_list=True,
+                        filename='exported_maps.pkl'):
+    """
+
+    :param n: number of beatmaps to fetch.
+    :param min_sr: lowest star rating
+    :param max_sr: highest star rating
+    :param random: shuffle entries
+    :param use_exported: Use already exported list
+    :param export_list: export the query to [filename]
+    :param filename: Where to save the exported list
+    :return:
+    """
+    pass
